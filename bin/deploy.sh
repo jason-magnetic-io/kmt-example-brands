45dbbb45db1c9385c778e4ede49e11bd55f6e9b3
Jason Griffin <jason@magnetic.io>
2019-05-29 11:49:43 +0200

    GV-40 added global update flag

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,24 +2,28 @@
 
 set -e
 
-for brand in $(ls brands)
-do
-	for config in $(ls brands/$brand/*.yaml)
+if [ -e updated ]
+then
+	for brand in $(ls brands)
 	do
-		context=$(basename $config | cut -d'.' -f1)
-		environment=$(echo $context | cut -d'-' -f2)
-		echo "Updating $brand $environment"
-		set +e
-		updated=true
-		python bin/copy_services.py \
-		  --config $config \
-		  --src-path kmt-example-service-catalog \
-		  --dst-path brands/$brand/production/services || updated=false
-		set -e
-		if [ "$updated" = true ]
-		then
-      kubectl config use-context $context
-      kustomize build brands/$brand/$environment | kubectl apply -f -
-		fi
+		for config in $(ls brands/$brand/*.yaml)
+		do
+			context=$(basename $config | cut -d'.' -f1)
+			environment=$(echo $context | cut -d'-' -f2)
+			echo "Updating $brand $environment"
+			updated_flag=true
+			python bin/copy_services.py \
+				--config $config \
+				--src-path kmt-example-service-catalog \
+				--dst-path brands/$brand/production/services || updated_flag=false
+			if [ "$updated_flag" = true ]
+			then
+				kubectl config use-context $context
+				kustomize build brands/$brand/$environment | kubectl apply -f -
+			fi
+		done
 	done
-done
+	rm updated
+else
+	echo "Nothing to update"
+fi
