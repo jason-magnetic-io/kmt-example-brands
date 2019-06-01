41d5256cd17a550a0648893a8622831e7d05b771
Jason Griffin <jason@magnetic.io>
2019-06-01 02:02:18 +0200

    Added logic for updating config

--- bin/update-vamp-config.sh
+++ bin/update-vamp-config.sh
@@ -11,19 +11,38 @@ do
 	esac
 done
 
-ENVSUBST_SHELL_FORMAT=\$VAMP_FORKLIFT_MYSQL_USER,\$VAMP_FORKLIFT_MYSQL_PASSWORD,\$VAMP_FORKLIFT_MYSQL_HOST,\$VAMP_FORKLIFT_SECURITY_LOOKUP_HASH_SALT,\$VAMP_FORKLIFT_SECURITY_PASSWORD_HASH_SALT
+# set envsubst variable list
+export ENVSUBST_SHELL_FORMAT=\$VAMP_FORKLIFT_MYSQL_USER,\$VAMP_FORKLIFT_MYSQL_PASSWORD,\
+\$VAMP_FORKLIFT_MYSQL_HOST,\$VAMP_FORKLIFT_SECURITY_LOOKUP_HASH_SALT,\$VAMP_FORKLIFT_SECURITY_PASSWORD_HASH_SALT
 
 cd $BASE_PATH
 
 cp forklift/config.yaml ~/.forklift
 
-echo "Creating Vamp config for $ORG $ENV"
 mkdir -p tmp
-envsubst ${ENVSUBST_SHELL_FORMAT} < organization.yaml > tmp/organization.yaml
-forklift create organization $ORG --file tmp/organization.yaml
-#forklift create user org-admin --role admin --organization org
-envsubst ${ENVSUBST_SHELL_FORMAT} < environment.yaml > tmp/environment.yaml
-forklift create environment $ENV --organization $ORG --file tmp/environment.yaml
+rm -rf tmp/*
+
+echo "Creating Vamp config for $ORG $ENV"
+if [ "$(forklift list organizations | grep $ORG)" == "" ]
+then
+  echo "Creating $ORG"
+	envsubst ${ENVSUBST_SHELL_FORMAT} < organization.yaml > tmp/organization.yaml
+	forklift create organization $ORG --file tmp/organization.yaml
+else
+  echo "Updating $ORG"
+	envsubst ${ENVSUBST_SHELL_FORMAT} < organization.yaml > tmp/organization.yaml
+	forklift update organization $ORG --file tmp/organization.yaml
+fi
+if [ "$(forklift list environments --organization $ORG | grep $ENV)" == "" ]
+then
+  echo "Creating $ENV"
+  envsubst ${ENVSUBST_SHELL_FORMAT} < environment.yaml > tmp/environment.yaml
+  forklift create environment $ENV --organization $ORG --file tmp/environment.yaml
+else
+  echo "Updating $ENV"
+  envsubst ${ENVSUBST_SHELL_FORMAT} < environment.yaml > tmp/environment.yaml
+  forklift update environment $ENV --organization $ORG --file tmp/environment.yaml
+fi
 
 for workflow in $(ls workflows)
 do
@@ -31,3 +50,5 @@ do
   forklift add artifact $workflow --organization $ORG --environment $ENV --file workflows/$workflow/breed.yaml
   forklift add artifact $workflow --organization $ORG --environment $ENV --file workflows/$workflow/workflow.yaml
 done
+
+#forklift create user org-admin --role admin --organization org
