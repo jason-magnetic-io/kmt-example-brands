6f52cdddfdee8722515b3b0e91b01f625de85cf2
Jason Griffin <jason@magnetic.io>
2019-06-04 15:37:16 +0200

    Changed indents to use spaces

--- bin/update.sh
+++ bin/update.sh
@@ -6,43 +6,43 @@ if [ -e updated ]
 then
   vamp_flag_file=$(pwd)/vamp-updated
 
-	for brand in $(ls brands)
-	do
-		for config in $(ls brands/$brand/*.yaml)
-		do
-			context=$(basename $config | cut -d'.' -f1)
-			environment=$(echo $context | cut -d'-' -f2)
+  for brand in $(ls brands)
+  do
+    for config in $(ls brands/$brand/*.yaml)
+    do
+      context=$(basename $config | cut -d'.' -f1)
+      environment=$(echo $context | cut -d'-' -f2)
 
-			echo "Updating $brand $environment"
-			
-			cp -r common brands/$brand/$environment/infrastructure/vamp
-			
-			bin/update-vamp-config.sh \
-			  -p brands/$brand/$environment/infrastructure/vamp \
-			  -o $brand \
-			  -e $environment \
-			  -f $vamp_flag_file
+      echo "Updating $brand $environment"
+      
+      cp -r common brands/$brand/$environment/infrastructure/vamp
+      
+      bin/update-vamp-config.sh \
+        -p brands/$brand/$environment/infrastructure/vamp \
+        -o $brand \
+        -e $environment \
+        -f $vamp_flag_file
 
-			vamp-kmt \
-			  --service-defs kmt-example-service-catalog \
-			  --application kmt-example-applications/$brand/$environment.yaml \
-			  --environment $config \
-			  --output brands/$brand/$environment
+      vamp-kmt \
+        --service-defs kmt-example-service-catalog \
+        --application kmt-example-applications/$brand/$environment.yaml \
+        --environment $config \
+        --output brands/$brand/$environment
 
-			updated_flag=true
-			python3 bin/copy_services.py \
-				--config $config \
-				--src-path kmt-example-service-catalog \
-				--dst-path brands/$brand/$environment/services || updated_flag=false
+      updated_flag=true
+      python3 bin/copy_services.py \
+        --config $config \
+        --src-path kmt-example-service-catalog \
+        --dst-path brands/$brand/$environment/services || updated_flag=false
 
-			if [ "$updated_flag" = true ]
-			then
-				kubectl config use-context $context
-				kustomize build brands/$brand/$environment | kubectl apply -f -
-			fi
-		done
-	done
-	rm updated
+      if [ "$updated_flag" = true ]
+      then
+        kubectl config use-context $context
+        kustomize build brands/$brand/$environment | kubectl apply -f -
+      fi
+    done
+  done
+  rm updated
 else
-	echo "Nothing to update"
+  echo "Nothing to update"
 fi
